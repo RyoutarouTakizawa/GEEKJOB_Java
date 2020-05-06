e6bd3f550e469c4838f69d1954d6edc4a7ed28b4
RyoutarouTakizawa <rt.0624.hhs@icloud.com>
2020-05-07 01:45:20 +0900

    5月6日学習分追加

---
+++ JavaUserManagementSystem_ver1.0_Eclipse/src/jums/InsertResult.java
@@ -0,0 +1,72 @@
+package jums;
+
+import java.io.IOException;
+import java.util.Calendar;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+
+/**
+ * Servlet implementation class InsertResult
+ */
+@WebServlet("/InsertResult")
+public class InsertResult extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public InsertResult() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		try{
+			//セッションスタート
+			HttpSession session = request.getSession();
+			request.setCharacterEncoding("UTF-8");//セッションに格納する文字コードをUTF-8に変更
+			//直リンク防止
+			String accesschk = request.getParameter("ac");
+			if(accesschk ==null || (Integer)session.getAttribute("ac")!=Integer.parseInt(accesschk)){
+				throw new Exception("不正なアクセスです");
+			}
+
+			//ユーザー情報に対応したJavaBeansオブジェクトに格納していく
+			UserDataDTO userdata = new UserDataDTO();
+			userdata.setName((String)session.getAttribute("name"));
+			Calendar birthday = Calendar.getInstance();
+			userdata.setBirthday(birthday.getTime());
+			userdata.setType(Integer.parseInt((String)session.getAttribute("type")));
+			userdata.setTell((String)session.getAttribute("tell"));
+			userdata.setComment((String)session.getAttribute("comment"));
+
+			//DBへデータの挿入
+			UserDataDAO .getInstance().insert(userdata);
+
+			request.getRequestDispatcher("/insertresult.jsp").forward(request, response);
+		}catch(Exception e){
+			//データ挿入に失敗したらエラーページにエラー文を渡して表示
+			request.setAttribute("error", e.getMessage());
+			request.getRequestDispatcher("/error.jsp").forward(request, response);
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
